b181b95483aade496f478b3d5d3ed233623b1f43
Stephanie Tidwell <stephanie.tidwell.civ@gmail.com>
2023-03-22 23:10:07 -0400

    updates

--- components/Content.js
+++ components/Content.js
@@ -9,6 +9,9 @@ import web3 from "../public/web3.png";
 import web4 from "../public/web4.png";
 import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
+import SocialMediaApp from "../public/SocialMediaApp.png";
+import PersonalLibraryRepo from "../public/PersonalLibraryRepo.png";
+import eventsforecastplanner from "../public/eventsforecastplanner.png";
 
 const Content = () => {
   return (
@@ -16,8 +19,11 @@ const Content = () => {
       <div id="content">
         <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
         <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-          Since the beginning of my journey as a freelance designer and
-          developer, I've done remote work for
+          I offer a comprehensive range of services to help businesses and
+          individuals build robust and user-friendly web applications. With
+          expertise in front-end a nd back-end development, I can create
+          responsive and dynamic websites that meet the unique needs of each
+          client. future developer
           <span className="text-teal-500"> agencies </span>
           consulted for <span className="text-teal-500">startups </span>
           and collaborated with talanted people to create digital products for
@@ -32,8 +38,8 @@ const Content = () => {
         <div>
           <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
           <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-            Since the beginning of my journey as a freelance designer and
-            developer, I've done remote work for
+            Since the beginning of my journey as a designer and developer, I
+            plan on doing remote work for
             <span className="text-teal-500"> agencies </span>
             consulted for <span className="text-teal-500">startups </span>
             and collaborated with talanted people to create digital products for
@@ -94,8 +100,8 @@ const Content = () => {
         <div>
           <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
           <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-            Since the beginning of my journey as a freelance designer and
-            developer, I've done remote work for
+            Since the beginning of my journey as a designer and developer, I've
+            done remote work for
             <span className="text-teal-500"> agencies </span>
             consulted for <span className="text-teal-500">startups </span>
             and collaborated with talanted people to create digital products for
@@ -108,58 +114,37 @@ const Content = () => {
         </div>
         <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
           <div className="basis-1/3 flex-1 ">
-            <Image
-              className="rounded-lg object-cover"
-              width={"100%"}
-              height={"100%"}
-              layout="responsive"
-              src={web1}
-            />
+            <a href="https://github.com/StephTidwell/PersonalLibraryRepo">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={PersonalLibraryRepo}
+              />
+            </a>
           </div>
           <div className="basis-1/3 flex-1">
-            <Image
-              className="rounded-lg object-cover"
-              width={"100%"}
-              height={"100%"}
-              layout="responsive"
-              src={web2}
-            />
+            <a href="https://github.com/StephTidwell/social-media-app">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={SocialMediaApp}
+              />
+            </a>
           </div>
           <div className="basis-1/3 flex-1">
-            <Image
-              className="rounded-lg object-cover"
-              width={"100%"}
-              height={"100%"}
-              layout="responsive"
-              src={web3}
-            />
-          </div>
-          <div className="basis-1/3 flex-1">
-            <Image
-              className="rounded-lg object-cover"
-              width={"100%"}
-              height={"100%"}
-              layout="responsive"
-              src={web4}
-            />
-          </div>
-          <div className="basis-1/3 flex-1">
-            <Image
-              className="rounded-lg object-cover"
-              width={"100%"}
-              height={"100%"}
-              layout="responsive"
-              src={web5}
-            />
-          </div>
-          <div className="basis-1/3 flex-1">
-            <Image
-              className="rounded-lg object-cover"
-              width={"100%"}
-              height={"100%"}
-              layout="responsive"
-              src={web6}
-            />
+            <a href="https://github.com/StephTidwell/EventsForecastPlanner">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={eventsforecastplanner}
+              />
+            </a>
           </div>
         </div>
       </section>
